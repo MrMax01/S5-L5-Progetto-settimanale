79ff60234dfa5f29bb2e06b1f600f9fb18658ee4
MrMax01 <massimo.mauro00@gmail.com>
2023-07-29 16:38:24 +0200

    header animation show m but don't go back

--- script.js
+++ script.js
@@ -8,6 +8,35 @@ document.addEventListener("scroll", (event) => {
 });
 
 // const svg = document.querySelectorAll("g");
+const svg = document.querySelectorAll('[stroke-linecap="butt"]');
+const svgShowed = [];
+const cicleNumber = 100;
+let count = 0;
+const pickRandomM = () => {
+  const randomIndex = Math.floor(Math.random() * svg.length);
+  return svg[randomIndex];
+};
 
-// console.log(svg);
-// setInterval(() => {}, 500);
+const app = () => {
+  const randomM = pickRandomM();
+  console.log(randomM);
+  if (randomM.getAttribute("opacity") === "0") {
+    console.log("entra");
+    svgShowed.push(randomM);
+    randomM.setAttribute("opacity", "1");
+  } else {
+    app();
+  }
+};
+
+setInterval(() => {
+  if (count < cicleNumber) {
+    app();
+    count++;
+  } else {
+    while (count === 0) {
+      svgShowed[count].setAttribute("opacity", "0");
+      count--;
+    }
+  }
+}, 100);
